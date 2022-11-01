a66a6aa5f5298466928d46b39f5f6c744be67e84
Neha Singh <singh.neha2301@gmail.com>
2022-11-01 16:44:02 +0000

    Added unit test

--- Is_Korean_Show_test.py
+++ Is_Korean_Show_test.py
@@ -1,20 +1,19 @@
 import unittest
-import json
-from bool_functions import is_korean_show
+from KDrama_Classes import KoreanShows
 
-class Bool_function_test(unittest.TestCase):
+class IsKoreanShow_test(unittest.TestCase):
     def testbool_func_tvDict_is_Null(self):
         tv_dict = {'origin_country': []}
-        self.assertFalse(is_korean_show(tv_dict))
+        self.assertFalse(KoreanShows.is_korean_show(tv_dict))
 
     def testbool_func_tvDict_has_origincountry_Korean(self):
            tv_dict = {'origin_country': ['KR']}
-           self.assertTrue(is_korean_show(tv_dict))
+           self.assertTrue(KoreanShows.is_korean_show(tv_dict))
 
     def testbool_func_tvDict_has_origincountry_NonKorean(self):
            tv_dict = {'origin_country': ['US']}
-           self.assertFalse(is_korean_show(tv_dict))
+           self.assertFalse(KoreanShows.is_korean_show(tv_dict))
 
     def testbool_func_tvDict_has_origincountry_KoreanWithManyContry(self):
            tv_dict = {'origin_country': ['KR','US','UK','IND']}
-           self.assertTrue(is_korean_show(tv_dict))
+           self.assertTrue(KoreanShows.is_korean_show(tv_dict))
